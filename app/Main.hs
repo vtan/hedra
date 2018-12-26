c7339f9704c922d937c00cccf3079fbf02bc518b
Viktor Tanyi <tanyi.viktor@gmail.com>
2018-12-26 19:06:05 +0100

    Add executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,58 @@
-module Main where
+{-# LANGUAGE LambdaCase #-}
+
+module Main (main) where
+
+import qualified Hedra
+
+import Control.Applicative ((<|>))
+import Control.Monad (guard, when)
+import Data.Char (isDigit, isSpace)
+import Data.List (dropWhileEnd)
+import Data.Maybe (listToMaybe)
+import System.IO (hFlush, isEOF, stdout)
 
 main :: IO ()
-main = pure ()
+main = do
+  putStr "hedra> "
+  hFlush stdout
+  eof <- isEOF
+  if eof
+    then putStrLn "Exiting."
+    else do
+      line <- getLine
+      let trimmedLine = (dropWhile isSpace . dropWhileEnd isSpace) line
+      when ((not . null) trimmedLine) (processInput trimmedLine)
+      main
+  where
+    processInput :: String -> IO ()
+    processInput input =
+      case parseInput input of
+        Just (count, die) -> do
+          putStr (input ++ ": ")
+          Hedra.printRoll count die
+        Nothing ->
+          putStrLn "Invalid input. Valid formats: d4, 2d6, 1d100, 4df"
+
+parseInput :: String -> Maybe (Int, Hedra.Die)
+parseInput input =
+  listToMaybe $ do
+    (count, afterCount) <- reads input <|> [(1, input)]
+    guard (count >= 0)
+    (_, afterSeparator) <- parseSeparator afterCount
+    (die, _) <- parseDie afterSeparator
+    pure (count, die)
+
+parseSeparator :: String -> [((), String)]
+parseSeparator = \case
+  'd' : rest -> [((), rest)]
+  _ -> []
+
+parseDie :: String -> [(Hedra.Die, String)]
+parseDie = \case
+  "%" -> [(Hedra.Percent, "")]
+  "f" -> [(Hedra.Fudge, "")]
+  digits@(_:_) | all isDigit digits ->
+    case read digits of
+      0 -> []
+      positiveSides -> [(fromIntegral (positiveSides :: Int), "")]
+  _ -> []
